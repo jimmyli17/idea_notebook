6df175f0c77ad884d1ec3cfd6a31fca5f3297a74
Jimmy Li <li17.jimmy@gmail.com>
2015-07-29 15:50:24 +0000

    add_starter_generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the User resource:
+  # CREATE
+  get '/new_user' => 'users#new'
+  get '/create_user' => 'users#create'
+
+  # READ
+  get '/users' => 'users#index'
+  get '/users/:id' => 'users#show'
+
+  # UPDATE
+  get '/users/:id/edit' => 'users#edit'
+  get '/users/:id/update' => 'users#update'
+
+  # DELETE
+  get '/users/:id/destroy' => 'users#destroy'
+  #------------------------------
+
   get '/idea_notebook/new' => 'ideas#new'
   get '/idea_notebook/create' => 'ideas#create'
   get '/idea_notebook/update/:id' => 'ideas#update'
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728182434) do
+ActiveRecord::Schema.define(version: 20150729135532) do
 
   create_table "ideas", force: :cascade do |t|
     t.string   "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
